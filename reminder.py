4aa81e128cc9b67fd7086899a043cf45ad3174a8
natsatra <mathangikcs@gmail.com>
2026-02-25 02:00:06 +0530

    Update reminder.py

--- reminder.py
+++ reminder.py
@@ -1,7 +1,8 @@
 import os
 import json
 import asyncio
-from datetime import date
+import pytz
+from datetime import datetime
 from telegram import Bot
 
 TOKEN = os.environ["TELEGRAM_TOKEN"]
@@ -11,7 +12,8 @@ async def send():
     with open("reminders.json") as f:
         reminders = json.load(f)
     
-    today = date.today().isoformat()  # format: "2026-03-01"
+   IST = pytz.timezone("Asia/Kolkata")
+   today = datetime.now(IST).date().isoformat() # format: "2026-03-01"
     if today in reminders:
         bot = Bot(token=TOKEN)
         await bot.send_message(chat_id=CHAT_ID, text=reminders[today])
