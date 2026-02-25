62f6da3821e6fe8f26d4284263e7330c854daf6e
Matty <mathangikcs@gmail.com>
2026-02-25 22:40:17 +0530

    Update reminder.py
    Changing parse mode to HTML

--- reminder.py
+++ reminder.py
@@ -19,7 +19,7 @@ async def send():
         message = reminders[today]
         bot = Bot(token=TOKEN)
         for chat_id in CHAT_IDS:
-            await bot.send_message(chat_id=chat_id.strip(), text=message, parse_mode="Markdown")
+            await bot.send_message(chat_id=chat_id.strip(), text=message, parse_mode="HTML")
     else:
         print(f"No reminder for {today}")
 
