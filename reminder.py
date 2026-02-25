4449e0a8b98ccefd3479c065309f8e8d58129426
natsatra <mathangikcs@gmail.com>
2026-02-25 16:21:07 +0530

    Update reminder.py
    changing the script to do the sheet to json conversion

--- reminder.py
+++ reminder.py
@@ -6,16 +6,20 @@ from datetime import datetime
 from telegram import Bot
 
 TOKEN = os.environ["TELEGRAM_TOKEN"]
-CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+CHAT_IDS = os.environ["TELEGRAM_CHAT_IDS"].split(",")
 
 async def send():
     with open("reminders.json") as f:
         reminders = json.load(f)
+
     IST = pytz.timezone("Asia/Kolkata")
     today = datetime.now(IST).date().isoformat()
+
     if today in reminders:
+        message = reminders[today]
         bot = Bot(token=TOKEN)
-        await bot.send_message(chat_id=CHAT_ID, text=reminders[today])
+        for chat_id in CHAT_IDS:
+            await bot.send_message(chat_id=chat_id.strip(), text=message)
     else:
         print(f"No reminder for {today}")
 
