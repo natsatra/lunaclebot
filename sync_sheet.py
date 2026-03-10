561aa0ffed555f8c3a95105115a8fe6545dc3d3e
Matty <mathangikcs@gmail.com>
2026-03-11 01:25:34 +0530

    Update sync_sheet.py

--- sync_sheet.py
+++ sync_sheet.py
@@ -7,8 +7,7 @@ import time
 SHEET_ID = os.environ["SHEET_ID"]
 
 def fetch_tab(tab_name, retries=3, delay=5):
-    # url = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={tab_name}" testing
-    url = f"...{SHEET_ID}..."
+     url = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={tab_name}" testing
 
     for attempt in range(retries):
         try:
